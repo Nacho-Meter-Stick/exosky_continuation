3c79937d805423fdbaf82eb426953a1cc5422138
Crimson <lorenzo.marez412@gmail.com>
2024-10-06 13:27:01 -0600

    saves planet name in image now

--- pygame_test.py
+++ pygame_test.py
@@ -84,9 +84,9 @@ while True:
         if start_button.collidepoint(pos):
             start = True
     else:
+        text_rect = planet_text.get_rect(center=(size[0]/2, 120))
+        window_surface.blit(planet_text, text_rect)
         if not(saving):
-            text_rect = planet_text.get_rect(center=(size[0]/2, 120))
-            window_surface.blit(planet_text, text_rect)
             switch_button = pygame.draw.rect(window_surface, '#F39237', pygame.Rect(1435, 15, 355, 60), 0, 3) 
             window_surface.blit(switch_text, (1450, 20))
             constellation_button = pygame.draw.rect(window_surface, '#F39237', pygame.Rect(70, 15, 345, 60), 0, 3)
@@ -113,7 +113,7 @@ while True:
             exit()
         j = 0;
         for i in range(int(len(arr)/4)):
-            pygame.draw.line(window_surface, '#F39237', (arr[j], arr[j+1]), (arr[j+2], arr[j+3]), 2)
+            pygame.draw.line(window_surface, '#F39237', (arr[j], arr[j+1]), (arr[j+2], arr[j+3]), 3)
             j+=4
         if saving:
             pygame.image.save(window_surface, "image.png")
