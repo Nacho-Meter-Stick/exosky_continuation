057cdb6933c9afbc9d339150ddc5d7dd2a0d3b34
Nacho-Meter-Stick <138950899+Nacho-Meter-Stick@users.noreply.github.com>
2024-10-06 13:14:03 -0600

    Update sphere_to_2circles.py

--- sphere_to_2circles.py
+++ sphere_to_2circles.py
@@ -10,3 +10,12 @@ def sphere_to_circle(sphere_xyzs: list[tuple[float, float, float]]) -> tuple[lis
         if c > 0: disc_points_top.append((-a/(c+1), b/(c+1)))
         else: disc_points_bottom.append((-a/(c-1), -b/(c-1)))
     return disc_points_top, disc_points_bottom
+def cartesian_STAR_MAP_to_circles(star_map) -> tuple[list[tuple[float, float, float]], list[tuple[float, float, float]]]:
+    disc_points_top = []
+    disc_points_bottom = []
+    for entry in star_map:
+        x, y, z = entry['coordinates']
+        mag = entry['magnitude']
+        if z > 0: disc_points_top.append((-x/(z+1), y/(z+1), mag))
+        else: disc_points_bottom.append((-x/(z-1), -y/(z-1), mag))
+    return disc_points_top, disc_points_bottom
