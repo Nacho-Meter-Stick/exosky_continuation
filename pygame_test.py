d1bc279d74ff1716de349dc4afe14ebec6fdc5ad
aagowl <abigail.gatsch@gmail.com>
2024-10-06 14:12:04 -0600

    fix bugs

--- pygame_test.py
+++ pygame_test.py
@@ -108,7 +108,7 @@ while True:
         text_rect = planet_text.get_rect(center=(size[0]/2, 120))
         window_surface.blit(planet_text, text_rect)
         if not(saving):
-            exoPlanetSelector.draw(screen) 
+            exoPlanetSelector.draw(window_surface) 
             window_surface.blit(switch_text, (1450, 20))
             constellation_button = pygame.draw.rect(window_surface, '#F39237', pygame.Rect(70, 15, 345, 60), 0, 3)
             window_surface.blit(constellation_text, (85, 20))
