bded3413985c1b8b6edc451aa8467e9fc557c147
Crimson <lorenzo.marez412@gmail.com>
2024-10-06 08:12:27 -0600

    Added exit and save buttons

--- pygame_test.py
+++ pygame_test.py
@@ -5,31 +5,31 @@ white = (255, 255, 255)
 pygame.init()
 start_font = pygame.font.Font('./programFonts/font.ttf', 100)
 planet_font = pygame.font.Font('./programFonts/font.ttf', 50)
-title = start_font.render('Exosky Challenge!', False, '#907AD6')
+title_text = start_font.render('Exosky Challenge!', False, '#907AD6')
 start_text = start_font.render('Start', False, '#EEE3CE')
 switch_text = planet_font.render('Switch Planets', False, '#907AD6')
+exit_text = planet_font.render('Exit', False, '#907AD6')
+save_text = planet_font.render('Save', False, '#907AD6')
 
 pygame.display.set_caption('Exosky!')
 window_surface = pygame.display.set_mode((0, 0), pygame.FULLSCREEN) #or pygame.FULLSCREEN to have the window stay fullscreen (no bar at top) Two numbers indicate minimized size
 
 background = pygame.Surface((3000, 2000))
 background.fill(pygame.Color('#161525'))
-is_running = True
 pos = (-1, -1)
 mapping = False
 start = False
+saving = False
 planet_str = "Earth"
 constellation_str = 'Start Charting'
 arr = []
 temp_len = 0
 
-while is_running:
+while True:
     window_surface.blit(background, (0, 0))
     planet_text = planet_font.render(planet_str, False, '#F39237')
     constellation_text = planet_font.render(constellation_str, False, '#907AD6')
     for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            is_running = False
         if event.type == pygame.MOUSEBUTTONDOWN:
             if(mapping):
                 if(pos[0] != -1):
@@ -45,20 +45,24 @@ while is_running:
         pygame.draw.circle(window_surface, color,[randint(0, 1920), i], randint(1, 4), 0)
         pygame.draw.circle(window_surface, color,[randint(0, 1920), i], randint(1, 4), 0)
         pygame.draw.circle(window_surface, color,[randint(0, 1920), i], randint(1, 4), 0)
-    pygame.image.save(window_surface, "image.png")
     if not(start):
         start_button = pygame.draw.rect(window_surface,'#F39237', pygame.Rect(810, 710, 280, 95),  0, 3)
-        window_surface.blit(title, (525, 450))
+        window_surface.blit(title_text, (525, 450))
         window_surface.blit(start_text, (825, 705))
         if start_button.collidepoint(pos):
             start = True
     else:
-        text_rect = planet_text.get_rect(center=(1920/2, 40))
-        window_surface.blit(planet_text, text_rect)
-        switch_button = pygame.draw.rect(window_surface, '#F39237', pygame.Rect(1435, 15, 390, 70), 0, 3) 
-        window_surface.blit(switch_text, (1450, 20))
-        constellation_button = pygame.draw.rect(window_surface, '#F39237', pygame.Rect(70, 15, 370, 70), 0, 3)
-        window_surface.blit(constellation_text, (85, 20))
+        if not(saving):
+            text_rect = planet_text.get_rect(center=(1920/2, 40))
+            window_surface.blit(planet_text, text_rect)
+            switch_button = pygame.draw.rect(window_surface, '#F39237', pygame.Rect(1435, 15, 390, 70), 0, 3) 
+            window_surface.blit(switch_text, (1450, 20))
+            constellation_button = pygame.draw.rect(window_surface, '#F39237', pygame.Rect(70, 15, 370, 70), 0, 3)
+            window_surface.blit(constellation_text, (85, 20))
+            exit_button = pygame.draw.rect(window_surface, '#F39237', pygame.Rect(90, 1115, 130, 70), 0, 3)
+            window_surface.blit(exit_text, (100, 1120))
+            save_button = pygame.draw.rect(window_surface, '#F39237', pygame.Rect(1710, 1115, 120, 70), 0, 3)
+            window_surface.blit(save_text, (1720, 1120))
         if switch_button.collidepoint(pos):
             print("planet switch!!")
             pos = (-1, -1)
@@ -73,8 +77,16 @@ while is_running:
                 if temp_len != len(arr):
                     arr.pop();arr.pop();arr.pop();arr.pop();
             pos = (-1, -1)
+        if exit_button.collidepoint(pos):
+            exit()
         j = 0;
         for i in range(int(len(arr)/4)):
-            pygame.draw.line(window_surface, '#F39237', (arr[j], arr[j+1]), (arr[j+2], arr[j+3]), 3)
+            pygame.draw.line(window_surface, '#F39237', (arr[j], arr[j+1]), (arr[j+2], arr[j+3]), 2)
             j+=4
+        if saving:
+            pygame.image.save(window_surface, "image.png")
+            saving = False
+        if save_button.collidepoint(pos):
+            saving = True
+            pos = (-1, -1)
     pygame.display.update()
