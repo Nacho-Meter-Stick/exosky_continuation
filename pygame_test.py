42c4b20b4f542d1589ffed0dde3b5a522eb91200
Crimson <lorenzo.marez412@gmail.com>
2024-10-05 20:57:59 -0600

    Updates to Pygame saves a png ignore this for now

--- pygame_test.py
+++ pygame_test.py
@@ -1,25 +1,40 @@
 import pygame
 from random import randint
-import time
 
 white = (255, 255, 255)
 pygame.init()
-my_font = pygame.font.Font('./programFonts/font.ttf', 100)
-text_surface = my_font.render('Exosky Challenge!', False, (153, 50, 204))
+start_font = pygame.font.Font('./programFonts/font.ttf', 100)
+planet_font = pygame.font.Font('./programFonts/font.ttf', 50)
+title = start_font.render('Exosky Challenge!', False, '#907AD6')
+start_text = start_font.render('Start', False, '#EEE3CE')
+switch_text = planet_font.render('Switch Planets', False, '#907AD6')
 
 pygame.display.set_caption('Exosky!')
 window_surface = pygame.display.set_mode((0, 0), pygame.FULLSCREEN) #or pygame.FULLSCREEN to have the window stay fullscreen (no bar at top) Two numbers indicate minimized size
 
 background = pygame.Surface((3000, 2000))
-background.fill(pygame.Color('#000000'))
+background.fill(pygame.Color('#161525'))
 is_running = True
+pos = (-1, -1)
+mapping = False
+start = False
+planet_str = "Earth"
+constellation_str = 'Start Charting'
+arr = []
+temp_len = 0
 
 while is_running:
+    window_surface.blit(background, (0, 0))
+    planet_text = planet_font.render(planet_str, False, '#F39237')
+    constellation_text = planet_font.render(constellation_str, False, '#907AD6')
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             is_running = False
-    window_surface.blit(background, (0, 0))
-    pygame.draw.circle(window_surface, white,[1500, 300], 5, 0)  #surface, Color, coord, radius, line thickness where 0 is solid
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            if(mapping):
+                if(pos[0] != -1):
+                    arr.append(pos[0]);arr.append(pos[1]);arr.append(pygame.mouse.get_pos()[0]);arr.append(pygame.mouse.get_pos()[1])
+            pos = pygame.mouse.get_pos()
     #Drawing Star:
          #0, 0 is top left
          #1920, 1200 is bot right (computer resolution) for full screen
@@ -30,7 +45,36 @@ while is_running:
         pygame.draw.circle(window_surface, color,[randint(0, 1920), i], randint(1, 4), 0)
         pygame.draw.circle(window_surface, color,[randint(0, 1920), i], randint(1, 4), 0)
         pygame.draw.circle(window_surface, color,[randint(0, 1920), i], randint(1, 4), 0)
-    window_surface.blit(text_surface, (525, 450))
+    pygame.image.save(window_surface, "image.png")
+    if not(start):
+        start_button = pygame.draw.rect(window_surface,'#F39237', pygame.Rect(810, 710, 280, 95),  0, 3)
+        window_surface.blit(title, (525, 450))
+        window_surface.blit(start_text, (825, 705))
+        if start_button.collidepoint(pos):
+            start = True
+    else:
+        text_rect = planet_text.get_rect(center=(1920/2, 40))
+        window_surface.blit(planet_text, text_rect)
+        switch_button = pygame.draw.rect(window_surface, '#F39237', pygame.Rect(1435, 15, 390, 70), 0, 3) 
+        window_surface.blit(switch_text, (1450, 20))
+        constellation_button = pygame.draw.rect(window_surface, '#F39237', pygame.Rect(70, 15, 370, 70), 0, 3)
+        window_surface.blit(constellation_text, (85, 20))
+        if switch_button.collidepoint(pos):
+            print("planet switch!!")
+            pos = (-1, -1)
+        if constellation_button.collidepoint(pos):
+            if constellation_str == 'Start Charting':
+                constellation_str = 'End Charting'
+                mapping = True
+                temp_len = len(arr)
+            else:
+                constellation_str = 'Start Charting'
+                mapping = False
+                if temp_len != len(arr):
+                    arr.pop();arr.pop();arr.pop();arr.pop();
+            pos = (-1, -1)
+        j = 0;
+        for i in range(int(len(arr)/4)):
+            pygame.draw.line(window_surface, '#F39237', (arr[j], arr[j+1]), (arr[j+2], arr[j+3]), 3)
+            j+=4
     pygame.display.update()
-    time.sleep(10)
-
