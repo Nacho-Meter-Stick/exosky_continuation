aa370279b09423378eabb1d1fe41ffb99237a3c4
aagowl <abigail.gatsch@gmail.com>
2024-10-06 23:09:50 -0600

    clear out unused classes

--- dropdown.py
+++ dropdown.py
@@ -31,8 +31,8 @@ class SearchableDropDown():
         self.menu_active = False
         self.draw_dropdown = False
         self.active_option = -1
-        self.text_surf = planet_font.render(text, True, self.menu_color[self.menu_active])
-        self.chosen_text_surf = planet_font.render(self.chosen, True, self.menu_color[self.menu_active])
+        self.text_surf = self.font.render(text, True, self.menu_color[self.menu_active])
+        self.chosen_text_surf = self.font.render(self.chosen, True, self.menu_color[self.menu_active])
 
     def draw(self, surf):
         pygame.draw.rect(surf, self.menu_color[self.menu_active], self.rect, 0)
@@ -104,96 +104,6 @@ class SearchableDropDown():
 
     def getChosen():
         return self.chosen
-
-
-
-class DropDown():
-    def __init__(self, menu_color, option_color, x, y, w, h, default, options):
-        self.menu_color = menu_color
-        self.option_color = option_color
-        self.rect = pygame.Rect(x, y, w, h)
-        self.options = options
-        self.chosen = default
-        self.font = pygame.font.SysFont(None, 30)
-        self.menu_active = False
-        self.draw_dropdown = False
-        self.active_option = -1
-
-    def draw(self, surf):
-        pygame.draw.rect(surf, self.menu_color[self.menu_active], self.rect, 0)
-        text = self.font.render(self.chosen, 1, (0, 0, 0))
-        surf.blit(text, text.get_rect(center = self.rect.center))
-
-        if self.draw_dropdown:
-            for i, text in enumerate(self.options):
-                rect = self.rect.copy()
-                rect.y += (i+1) * self.rect.height
-                pygame.draw.rect(surf, self.option_color[1 if i == self.active_option else 0], rect, 0)
-                text = self.font.render(text, 1, (0, 0, 0))
-                surf.blit(text, text.get_rect(center = rect.center))
-
-
-    def update(self, event):
-        mpos = pygame.mouse.get_pos()
-        self.menu_active = self.rect.collidepoint(mpos)
-
-        for i in range(len(self.options)):
-            rect = self.rect.copy()
-            rect.y += (i+1) * self.rect.height
-            if rect.collidepoint(mpos):
-                self.active_option = i
-                break
-
-        if not self.menu_active and self.active_option == -1:
-            self.draw_dropdown = False
-
-        for event in event_list:
-            if event.type == pygame.MOUSEBUTTONDOWN:
-                if self.menu_active:
-                    self.draw_dropdown = not self.draw_dropdown
-                elif self.draw_dropdown and self.active_option >= 0:
-                    self.draw_dropdown = False
-                    return self.active_option
-        return -1
-
-class UserInputBox():
-    def __init__(self, x, y, w, h, text="Search an Exoplanet!"):
-        self.rect = pygame.Rect(x, y, w, h)
-        self.text = text
-        self.font = pygame.font.SysFont(None, 30)
-        self.active = False
-        self.color = COLOR_ACTIVE
-        self.txt_color = COLOR_INACTIVE
-        self.text_surf = FONT.render(text, True, self.color)
-
-    def update(self, event_list):
-        for event in event_list:
-            if event.type == pygame.MOUSEBUTTONDOWN:
-                if self.rect.collidepoint(event.pos):
-                    self.active = not self.active
-                    if self.active:
-                        self.text = ""
-                else:
-                    self.active = False
-            if event.type == pygame.KEYDOWN:
-                if not self.active:
-                    break
-                if event.key == pygame.K_BACKSPACE:
-                    self.text = self.text[:-1]
-                else:
-                    self.text += event.unicode
-                
-        
-        self.text_surf = self.font.render(self.text, True, self.txt_color)
-
-        # make box longer when text gets too big
-        width = max(200, self.text_surf.get_width()+10)
-        self.rect.w = width
-
-    def draw(self, surf):
-        pygame.draw.rect(surf, self.color, self.rect, 0, border_radius=3)
-        screen.blit(self.text_surf, (self.rect.x+5, self.rect.y+5))
-        
 
 if __name__ == "__main__":
     screen = pygame.display.set_mode((640, 480))
